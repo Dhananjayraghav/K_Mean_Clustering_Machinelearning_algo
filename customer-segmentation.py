8e668e26911a11dbe5842f62c1917acd5dfaf65f
Dhananjayraghav <tdhruvdhruv@gmail.com>
2025-04-14 16:48:52 +0530

    Update customer-segmentation.py

--- customer-segmentation.py
+++ customer-segmentation.py
@@ -1,1 +1,48 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
 
+
+customers_data = pd.read_csv('C:/Users/tdhru/.cache/kagglehub/datasets/vjchoudhary7/customer-segmentation-tutorial-in-python/Mall_Customers.csv')
+X = customers_data.iloc[:,[3,4]].values
+
+# finding the wcss value
+wcss = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+    kmeans.fit(X)
+
+    wcss.append(kmeans.inertia_)
+
+# plot an elbow graph
+sns.set()
+plt.plot(range(1,11),wcss)
+plt.title('The elbow Graph')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt .show()
+
+#training
+kmeans = KMeans(n_clusters=5, init='k-means++', random_state=0)
+y_kmeans = kmeans.fit_predict(X)
+
+print(y_kmeans)
+
+#Visualizing
+
+plt.figure(figsize=(8,8))
+plt.scatter(X[y_kmeans==0,0],X[y_kmeans==0,1],s=50,c='red',label='Cluster 1')
+plt.scatter(X[y_kmeans==1,0],X[y_kmeans==1,1],s=50,c='blue',label='Cluster 2')
+plt.scatter(X[y_kmeans==2,0],X[y_kmeans==2,1],s=50,c='green',label='Cluster 3')
+plt.scatter(X[y_kmeans==3,0],X[y_kmeans==3,1],s=50,c='cyan',label='Cluster 4')
+plt.scatter(X[y_kmeans==4,0],X[y_kmeans==4,1],s=50,c='magenta',label='Cluster 5')
+
+#plot the centroids
+
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=100,c='yellow',label='Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.show()  
